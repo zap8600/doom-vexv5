848206cc516d8473fc29f8ba24221535b7a9b2c2
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-01-15 14:49:16 -0600

    wip

--- doomgeneric/doomgeneric_vex.c
+++ doomgeneric/doomgeneric_vex.c
@@ -8,8 +8,6 @@
 
 #include <stdio.h>
 
-#include <Windows.h>
-
 static BITMAPINFO s_Bmi = { sizeof(BITMAPINFOHEADER), DOOMGENERIC_RESX, -DOOMGENERIC_RESY, 1, 32 };
 static HWND s_Hwnd = 0;
 static HDC s_Hdc = 0;
@@ -96,69 +94,11 @@ static LRESULT CALLBACK wndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
 
 void DG_Init()
 {
-	// window creation
-	const char windowClassName[] = "DoomWindowClass";
-	const char windowTitle[] = "Doom";
-	WNDCLASSEXA wc;
-
-	wc.cbSize = sizeof(WNDCLASSEXA);
-	wc.style = 0;
-	wc.lpfnWndProc = wndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = 0;
-	wc.hIcon = 0;
-	wc.hCursor = 0;
-	wc.hbrBackground = 0;
-	wc.lpszMenuName = 0;
-	wc.lpszClassName = windowClassName;
-	wc.hIconSm = 0;
-
-	if (!RegisterClassExA(&wc))
-	{
-		printf("Window Registration Failed!");
-
-		exit(-1);
-	}
-
-	RECT rect;
-	rect.left = rect.top = 0;
-	rect.right = DOOMGENERIC_RESX;
-	rect.bottom = DOOMGENERIC_RESY;
-	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
-
-	HWND hwnd = CreateWindowExA(0, windowClassName, windowTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rect.right - rect.left, rect.bottom - rect.top, 0, 0, 0, 0);
-	if (hwnd)
-	{
-		s_Hwnd = hwnd;
-
-		s_Hdc = GetDC(hwnd);
-		ShowWindow(hwnd, SW_SHOW);
-	}
-	else
-	{
-		printf("Window Creation Failed!");
-
-		exit(-1);
-	}
-
-	memset(s_KeyQueue, 0, KEYQUEUE_SIZE * sizeof(unsigned short));
+	Brain.Screen.setPenWidth(1);
 }
 
 void DG_DrawFrame()
 {
-	MSG msg;
-	memset(&msg, 0, sizeof(msg));
-
-	while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE) > 0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessageA(&msg);
-	}
-
-	StretchDIBits(s_Hdc, 0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY, 0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY, DG_ScreenBuffer, &s_Bmi, 0, SRCCOPY);
-
-	SwapBuffers(s_Hdc);
 }
 
 void DG_SleepMs(uint32_t ms)
